adee2d81e03dd1058ba291461ddd35d45eacce57
Abinabi2005 <abinarun03@gmail.com>
2025-06-08 21:49:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Home from './components/Home';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Flight from './components/Flight';
 
@@ -18,7 +18,6 @@ function App() {
            <Route path='/Home' element={<Home />} />
            <Route path='/' element={<Login />} />
            <Route path='/Flight' element={<Flight />} />
-           <Route path="/Home" element={<Navigate to="/Home" />} />
          </Routes>
          <Footer />
       </Router>
